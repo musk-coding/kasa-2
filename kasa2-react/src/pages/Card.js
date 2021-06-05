ec758f42d1a13390acc5e96483f5c248a5372984
othmane <othmane.abouelyzza@gmail.com>
2021-06-05 17:37:18 +0200

    Add loader to Card component

--- kasa2-react/src/pages/Card.js
+++ kasa2-react/src/pages/Card.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Loader } from "../components";
 
 function Card() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   const { id } = useParams();
+  // const [rents, setRents] = useState([]);
 
-  return <>CARD {id}</>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const rents = await (await getRents()).data["-Magcmd3knyXhPZ1CiDh"];
+        setIsLoaded(true);
+        // setRents(rents);
+      } catch (error) {
+        setIsLoaded(true);
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error...</div>;
+  } else if (!isLoaded) {
+    return <Loader />;
+  } else {
+    return <>CARD {id}</>;
+  }
 }
 
 export default Card;
